06c80c1b7ceecd7d3c25cd30ae812543b5a72887
Anantha Raju C <arcswdev@gmail.com>
2021-10-26 15:23:15 +0530

    Initial Commit: Project Created

---
+++ src/main/java/io/github/anantharajuc/sbwsb/SpringBatchWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.github.anantharajuc.sbwsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchWithSpringBootApplication.class, args);
+	}
+
+}
